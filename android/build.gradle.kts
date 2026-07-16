5cb4ef9550676c76ddd24a4230f1a3e677dba65c
SyedMuneebShah <muneebalishah764@gmail.com>
2026-07-16 14:39:44 +0500

    add : new changes

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,13 @@
+plugins {
+    id("org.sonarqube") version "7.3.1.8318"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "mm-sonarqube_RecipeProject_da673c85-2f36-4564-b4e4-4a893a3c7c31")
+        property("sonar.projectName", "RecipeProject")
+    }
+}
 allprojects {
     repositories {
         google()
